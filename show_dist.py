94b34769aa1ab5e1380c525f53c4d0e88e7d3c6d
jiakai <jia.kai66@gmail.com>
2015-04-19 22:51:35 +0800

    fix bugs in showdist

--- show_dist.py
+++ show_dist.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 # $File: show_dist.py
-# $Date: Sun Apr 19 22:23:31 2015 +0800
+# $Date: Sun Apr 19 22:48:43 2015 +0800
 # $Author: jiakai <jia.kai66@gmail.com>
 
 from nasmia.utils import serial
@@ -26,6 +26,8 @@ class ShowDist(object):
     _ftr1_slice = None
 
     def __init__(self, img0, ftr0, img1, ftr1):
+        logger.info('img shape: {}; feature shape: {}'.format(
+            img0.shape, ftr0.shape))
         assert img0.shape == img1.shape and ftr0.shape == ftr1.shape
         assert ftr0.ndim == 4 and img0.ndim == 3
         for i in range(3):
@@ -45,10 +47,18 @@ class ShowDist(object):
     def _on_img0_click(self, x, y, z):
         logger.info('click at {}, slice={}'.format(
             (x, y, z), self._ftr1_slice))
+        x -= LAYER1_PATCH_SIZE / 2
+        y -= LAYER1_PATCH_SIZE / 2
+        z -= LAYER1_PATCH_SIZE / 2
+        if min(x, y, z, self._ftr0.shape[1] - max(x, y, z)) < 0:
+            logger.warn('click out of feature boundary')
+            return
         ftr0 = self._ftr0[:, x, y, z].reshape(1, -1)
-        rst = np.tensordot(ftr0, self._ftr1_sub, axis=(1, 0))
+        rst = np.tensordot(ftr0, self._ftr1_sub, axes=(1, 0))[0]
         fig = plt.figure(0)
-        fig.matshow(rst)
+        ax = fig.add_subplot(111)
+        matshow = ax.matshow(rst, interpolation='nearest')
+        plt.colorbar(matshow)
         fig.show()
 
     def _on_img1_axis_change(self, axis, pos, ind):
