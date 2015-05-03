07aecbf9cfec6a3a516f73039a8cf6f809adeffb
jiakai <jia.kai66@gmail.com>
2015-05-03 21:29:36 +0800

    support multiple distance measures in show_dist

--- show_dist.py
+++ show_dist.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 # $File: show_dist.py
-# $Date: Sat May 02 22:01:17 2015 +0800
+# $Date: Sun May 03 17:32:23 2015 +0800
 # $Author: jiakai <jia.kai66@gmail.com>
 
 from nasmia.utils import serial
@@ -22,8 +22,10 @@ class ShowDist(object):
     _ftr1_slice = None
     _img_shape = None
     _conv_shape = None
+    _dist_measure = None
 
-    def __init__(self, img0, ftr0, img1, ftr1):
+    def __init__(self, img0, ftr0, img1, ftr1, dist_measure):
+        self._dist_measure = dist_measure
         logger.info('img shape: {}; feature shape: {}'.format(
             img0.shape, ftr0.shape))
         assert img0.shape == img1.shape and ftr0.shape == ftr1.shape
@@ -67,12 +69,34 @@ class ShowDist(object):
         y -= border
         z -= border
         ftr0 = self._ftr0[:, x, y, z].reshape(-1, 1, 1)
-        rst = np.sqrt(np.square(ftr0 - self._ftr1_sub).sum(axis=0))
+        rst = self._get_dist(ftr0, self._ftr1_sub)
         fig = plt.figure(0)
         ax = fig.add_subplot(111)
         matshow = ax.matshow(rst, interpolation='nearest')
         plt.colorbar(matshow)
         fig.show()
+
+    def _get_dist(self, ref, ftrmap):
+        if self._dist_measure.startswith('l'):
+            norm = float(self._dist_measure[1:])
+            logger.info('using dist: L{}'.format(norm))
+            return np.power(
+                np.power(np.abs(ref - ftrmap), norm).sum(axis=0),
+                1.0 / norm)
+
+        if self._dist_measure == 'sparse':
+            logger.info('using dist: sparse')
+            return 1 - ((ref > 0) == (ftrmap > 0)).mean(axis=0)
+
+        if self._dist_measure == 'cos':
+            logger.info('using dist: cos')
+            norm_coeff = lambda v: 1.0 / np.sqrt(np.square(v).sum(
+                axis=0, keepdims=True))
+            ref = ref * norm_coeff(ref)
+            ftrmap = ftrmap * norm_coeff(ftrmap)
+            return 1 - (ref * ftrmap).sum(axis=0)
+
+        raise RuntimeError('unknown dist: {}'.format(self._dist_measure))
 
     def _on_img1_axis_change(self, axis, pos):
         border = self._conv_shape / 2
@@ -93,12 +117,15 @@ def main():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('pack0')
     parser.add_argument('pack1')
+    parser.add_argument('--measure', default='l2',
+                        help='distance measure')
     args = parser.parse_args()
 
     pack0 = serial.load(args.pack0)
     pack1 = serial.load(args.pack1)
     ShowDist(pack0['img'], pack0['ftr'],
-             pack1['img'], pack1['ftr'])
+             pack1['img'], pack1['ftr'],
+             args.measure)
 
 if __name__ == '__main__':
     main()
